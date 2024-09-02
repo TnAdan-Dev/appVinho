e782a146071ad7193e2984ce185ceeaf46227b70
TnAdan-Dev <ademirandrade2129@hotmail.com>
2024-09-02 11:08:17 -0300

    inicio do projeto

--- app/_layout.js
+++ app/_layout.js
@@ -2,8 +2,20 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
+    <Stack
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: "#010101",
+        borderBottomWidth: 0, // Remove a borda
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        fontWeight: "bold",
+      },
+      headerTitleAlign: "center", // Centraliza o título na barra de navegação
+    }}
+  >
+      <Stack.Screen name="index" options={{title: "WineWiz"}}/>
     </Stack>
   );
 }
--- app/index.js
+++ app/index.js
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function Index() {
+function HomeScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
